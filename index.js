4ec9eca797d4cd6d02add35e038cdc70ff4e8a1f
Yessi Angulo <alexanderangulo917@gmail.com>
2022-06-07 12:19:52 -0500

    ds

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express =  require('express')
 const app = express();
 
+// heroku git:remote -a test-db-yessi 
+// git push heroku master
+// heroku logs --tail
 
-
+console.log('arranca')
 // SERVER
 app.listen(process.env.PORT || 3001)
 console.log('server on port', process.env.PORT || 3001);
